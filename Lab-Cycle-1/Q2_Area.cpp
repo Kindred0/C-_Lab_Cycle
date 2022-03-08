503d018d239b5eafdfad9dc604d9c2261b6b813a
Lenat Thomas <95519891+Kindred0@users.noreply.github.com>
2022-03-08 21:28:11 +0530

    Add files via upload

--- Lab-Cycle-1/Q2_Area.cpp
+++ Lab-Cycle-1/Q2_Area.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 float area(int angle, float length, float breadth) // Area of a parrellogram
 {
+    angle=angle*(float(355)/113)/180;
     return (length*breadth*sin(angle));
 }
 float area(float length, float breadth) // Area of rectangle
@@ -15,11 +16,11 @@ float area(float side) // Area of square
 } 
 float area(double major, double minor) //Area of ellipse
 {
-    return (3.14*major*minor);
+    return ((float(355)/113)*major*minor);
 }
 float area(double radius)// Area of circle
 {
-    return (3.14*radius*radius);
+    return ((float(355)/113)*radius*radius);
 }
 float area(float a, float b, float c) // Area of a triangle with three sides
 {
@@ -28,6 +29,7 @@ float area(float a, float b, float c) // Area of a triangle with three sides
 }
 float area(float base, float side, int angle) // Area of trianlge with two sides and an angle between them
 {
+    angle=angle*(float(355)/113)/180;
     return (base*side*sin(angle)/2);
 }
 int main()
@@ -35,57 +37,57 @@ int main()
     int action, angle;
     float side1, side2, side3;
     double side4, side5;
-    cout<<" Actions\n\n";
-    cout<<" Parrellogram \t\t\t\t= 1 \n Rectangle \t\t\t\t= 2 \n Square \t\t\t\t= 3 \n Ellipse \t\t\t\t= 4 \n Circle \t\t\t\t= 5 \n Triangle with three sides \t\t= 6 \n Triangle with two sides and an angle \t= 7";
-    cout<<"\n\n Enter the shape to find the area \t= ";
+    cout<<"Actions\n\n";
+    cout<<"1\t: Parellogram\n2\t: Rectangle\n3\t: Square\n4\t: Ellipse\n5\t: Circle\n6\t: Triangle with three sides\n7\t: Triangle with two sides and angle\n";
+    cout<<"\n\nEnter the shape to find the area \t= ";
     cin>>action;
     switch(action)
     {
-        case 1: cout<<"\n Base      : ";
+        case 1: cout<<"\nBase              : ";
                 cin>>side1;
-                cout<<"\n Side      : ";
+                cout<<"\nSide              : ";
                 cin>>side2;
-                cout<<"\n Angle     : ";
+                cout<<"\nAngle in degree   : ";
                 cin>>angle;
-                cout<<"\n Area is   : "<<area(angle, side1, side2)<<endl;
+                cout<<"\nArea is   : "<<area(angle, side1, side2)<<endl;
                 break;
-        case 2: cout<<"\n Length    : ";
+        case 2: cout<<"\nLength    : ";
                 cin>>side1;
-                cout<<"\n Breadth   : ";
+                cout<<"\nBreadth   : ";
                 cin>>side2;
-                cout<<"\n Area is   : "<<area(side1, side2)<<endl;
+                cout<<"\nArea is   : "<<area(side1, side2)<<endl;
                 break;
-        case 3: cout<<"\n Side      : ";
+        case 3: cout<<"\nSide      : ";
                 cin>>side1;
-                cout<<"\n Area is   : "<<area(side1)<<endl;
+                cout<<"\nArea is   : "<<area(side1)<<endl;
                 break;
-        case 4: cout<<"\n Major     : ";
+        case 4: cout<<"\nMajor     : ";
                 cin>>side4;
-                cout<<"\n Minor     : ";
+                cout<<"\nMinor     : ";
                 cin>>side5;
-                cout<<"\n Area is   : "<<area(side4, side5)<<endl;
+                cout<<"\nArea is   : "<<area(side4, side5)<<endl;
                 break;
-        case 5: cout<<"\n Radius    : ";
+        case 5: cout<<"\nRadius    : ";
                 cin>>side4;
-                cout<<"\n Area is   : "<<area(side4)<<endl;
+                cout<<"\nArea is   : "<<area(side4)<<endl;
                 break;
-        case 6: cout<<"\n side 1    : ";
+        case 6: cout<<"\nside 1    : ";
                 cin>>side1;
-                cout<<"\n side 2    : ";
+                cout<<"\nside 2    : ";
                 cin>>side2;
-                cout<<"\n side 3    : ";
+                cout<<"\nside 3    : ";
                 cin>>side3;
-                cout<<"\n Area is   : "<<area(side1, side2, side3)<<endl;
+                cout<<"\nArea is   : "<<area(side1, side2, side3)<<endl;
                 break;
-        case 7: cout<<"\n Base      : ";
+        case 7: cout<<"\nBase              : ";
                 cin>>side1;
-                cout<<"\n Side      : ";
+                cout<<"\nSide              : ";
                 cin>>side2;
-                cout<<"\n Angle     : ";
+                cout<<"\nAngle in degree   : ";
                 cin>>angle;
-                cout<<"\n Area is   : "<<area(angle, side1, side2)<<endl;
+                cout<<"\nArea is   : "<<area(angle, side1, side2)<<endl;
                 break;
-        default:cout<<"\n Invalid input";
+        default:cout<<"\nInvalid input";
                 break;
     }
     return 0;
