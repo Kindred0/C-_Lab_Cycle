fb0ea724849679305e55328364f7562b665a0965
Lenat Thomas <95519891+Kindred0@users.noreply.github.com>
2022-03-10 21:52:25 +0530

    Add files via upload

--- Lab-Cycle-1/Q10_shopping.cpp
+++ Lab-Cycle-1/Q10_shopping.cpp
@@ -7,15 +7,20 @@ class item
     float price,total;
     string name;
     public:
-    void getdata(int a,string b,float c,int d)
+    void getdata(void)
     {
-        code=a;
-        price=c;
-        name=b;
-        quantity=d;
+        cout<<"\nCode\t\t: ";
+        cin>>code;
+        cout<<"Name\t\t: ";
+        cin>>name;
+        cout<<"Price\t\t: ";
+        cin>>price;
+        cout<<"Quantity\t: ";
+        cin>>quantity;
         total=price*quantity;
-        }
-    void printdata(void){
+    }
+    void printdata(void)
+    {
         cout<<"\n"<<code<<"\t\t\t\t"<<name<<"\t\t\t\t"<<price<<"\t\t\t\t"<<quantity;
     }
     int putcode(void)
@@ -54,115 +59,107 @@ void sort(item* list,int n)
 int main()
 {
     item *order,*temporder;
-    string name;
-    int action,n=0,code,quantity;
-    float price,total=0;
-    l1:
-    cout<<"\n\n1\t: Add an item to the cart\n2\t: Delete an item from the cart\n3\t: Show order\n4\t: Create the bill";
-    cout<<"\nEnter the action\t: ";
-    cin>>action;
-    if(action==1)
+    int action,code,n=0,stop=0;
+    float total=0;
+    while(stop==0)
     {
-        if(n==0)
+        cout<<"\n\n1\t: Add an item to the cart\n2\t: Delete an item from the cart\n3\t: Show order\n4\t: Create the bill";
+        cout<<"\nEnter the action\t: ";
+        cin>>action;
+        if(action==1)
         {
-            order=new item[1];
-            cout<<"\nCode\t\t: ";
-            cin>>code;
-            cout<<"\nName\t\t: ";
-            cin>>name;
-            cout<<"\nPrice\t\t: ";
-            cin>>price;
-            cout<<"\nQuantity\t:";
-            cin>>quantity;
-            order[0].getdata(code,name,price,quantity);
-            n=n+1;
-            total=total+order[0].puttotal();
-        }
-        else 
-        {
-            temporder=new item[n];
-            for(int i=0;i<n;i++)
+            if(n==0)
             {
-                temporder[i]=order[i];
+                order=new item[1];
+                order[0].getdata();
+                n=n+1;
             }
-            delete[] order;
-            order=new item[n+1];
-            for(int i=0;i<n;i++)
+            else 
             {
-                order[i]=temporder[i];
-            }
-            delete[] temporder;
-            cout<<"\nCode\t:";
-            cin>>code;
-            cout<<"\nName\t: ";
-            cin>>name;
-            cout<<"\nPrice\t: ";
-            cin>>price;
-            cout<<"\nQuantity: ";
-            cin>>quantity;
-            order[n].getdata(code,name,price,quantity);
-            n=n+1;
-            total=total+order[n].puttotal();
-            sort(order,n);
-        }
-    }
-    else if(action==2)
-    {
-        if(n==0)
-        {
-            cout<<"No items added to the cart yet, Add an item to your cart first";
-            goto l1;
-        }
-        cout<<"\nEnter the code numberof the item to be deleted";
-        cin>>code;
-        for(int i=0;i<n;i++)
-        {
-            if(code==order[i].putcode())
-            {
-                order[i].printdata();
-                cout<<"\nDeleted successfully";
-                total=total-order[i].puttotal();
-                order[i]=order[n-1];
-                n=n-1;
                 temporder=new item[n];
                 for(int i=0;i<n;i++)
                 {
                     temporder[i]=order[i];
                 }
                 delete[] order;
-                order=new item[n];
+                order=new item[n+1];
                 for(int i=0;i<n;i++)
                 {
                     order[i]=temporder[i];
                 }
                 delete[] temporder;
-                sort(order,n);               
+                order[n].getdata();
+                n=n+1;
+                sort(order,n);
             }
         }
-    }
-    else if(action==3)
-    {
-        cout<<"\nCode\t\t\t\tName\t\t\t\tPrice\t\t\t\tQuantity";
-        for(int i=0;i<n;i++)
+        else if(action==2)
         {
-            order[i].printdata();
+            if(n==0)
+            {
+                cout<<"\nNo items added to the cart yet, Add an item to your cart first";
+            }
+            else
+            {
+                cout<<"\nEnter the code numberof the item to be deleted\t: ";
+                cin>>code;
+                for(int i=0;i<n;i++)
+                {
+                    if(code==order[i].putcode())
+                    {
+                        order[i].printdata();
+                        cout<<"\nDeleted successfully";
+                        order[i]=order[n-1];
+                        n=n-1;
+                        temporder=new item[n];
+                        for(int i=0;i<n;i++)
+                        {
+                            temporder[i]=order[i];
+                        }
+                        delete[] order;
+                        order=new item[n];
+                        for(int i=0;i<n;i++)
+                        {
+                            order[i]=temporder[i];
+                        }
+                        delete[] temporder;
+                        stop=1;
+                        sort(order,n);
+                        break;
+                    }    
+                }
+                if(stop==0)
+                {
+                    cout<<"\nNo such item have added to the cart,Try again";
+                }
+                stop=0;
+            }
+        }
+        else if(action==3)
+        {
+            cout<<"\nCode\t\t\t\tName\t\t\t\tPrice\t\t\t\tQuantity";
+            for(int i=0;i<n;i++)
+            {
+                order[i].printdata();
+            }
+        }
+        else if(action==4)
+        {
+            cout<<"\nCode\t\t\t\tName\t\t\t\tPrice\t\t\t\tQuantity";
+            for(int i=0;i<n;i++)
+            {
+                order[i].printdata();
+                total=total+order[i].puttotal();
+            }
+            cout<<"\n\nTotal\t\t: "<<total;
+            cout<<"\n\nThank you for the purchase\n";
+            delete[] order;
+            cout<<"order deleted successfully";
+            return 0;
+        }
+        else
+        {
+            cout<<"\nInvalid input,Try again";
         }
     }
-    else if(action==4)
-    {
-        cout<<"\nCode\t\t\t\tName\t\t\t\tPrice\t\t\t\tQuantity";
-        for(int i=0;i<n;i++)
-        {
-            order[i].printdata();
-        }
-        cout<<"\nTotal\t\t\t\t\t\t\t\t: "<<total;
-        cout<<"\nThank you for the purchase\n";
-        return 0;
-    }
-    else
-    {
-        cout<<"\nInvalid input,Try again";
-        goto l1;
-    }
-    goto l1;
 }
