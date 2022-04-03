311613976a81253dbe1d27606f8bd8501cea57ab
Lenat Thomas <95519891+Kindred0@users.noreply.github.com>
2022-04-03 20:52:20 +0530

    Add files via upload

--- Lab-Cycle-3/Q3_exception.cpp
+++ Lab-Cycle-3/Q3_exception.cpp
@@ -1,106 +1,32 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
-class Matrix
+class test
 {
-    int row,colmn;
-    float **element;
+    float x,y;
     public:
-    Matrix()
+    test(){};
+    test(float a,float b){x=a;y=b;}
+    float devide(void)
     {
-        row=0;
-        colmn=0;
-        element=new float*[1];
+        if((x+y)==0)
+        throw("\nZero Devision");
+        return x/(x+y);
     }
-    ~Matrix();
-    void create(void);
-    void create(int,int);
-    void operator+(const Matrix &);
-    void display(void);
 };
-void Matrix::create()
-{
-    delete[] element;
-    cout<<"\nEnter row\t: ";
-    cin>>row;
-    cout<<"\nEnter coloumn\t: ";
-    cin>>colmn;
-    cout<<"\n";
-    element=new float*[row];
-    for(int i=0;i<colmn;i++)
-    {
-        element[i]=new float[row];
-        for(int j=0;j<row;j++)
-        {
-            cout<<"Enter at ["<<i+1<<"]["<<j+1<<"]\t: ";
-            cin>>element[i][j];
-        }
-    }
-}
-void Matrix::create(int r,int c)
-{
-    delete[] element;
-    row=r;
-    colmn=c;
-    element=new float*[row];
-    for(int i=0;i<row;i++)
-    {
-        element[i]=new float[colmn];
-    }
-}
-Matrix::~Matrix()
-{
-    for(int i=0;i<row;i++)
-        delete[] element[i];
-    delete element;
-}
-void Matrix::operator+(const Matrix &B)
-{
-    Matrix C;
-    if((row==B.row)&&(colmn==B.colmn))
-    {
-        C.create(row,row);
-        for(int i=0;i<C.row;i++)
-        {
-            for(int j=0;j<C.colmn;j++)
-            {
-                C.element[i][j]=element[i][j]+B.element[i][j];
-            }
-        }
-        C.display();
-    }
-    else 
-    throw("Number of rows and coloumns not the same therefore not possible to find the sum");
-}
-void Matrix::display(void)
-{
-    cout<<"\n";
-    for(int i=0;i<colmn;i++)
-    {
-        for(int j=0;j<row;j++)
-        {
-            cout<<element[i][j]<<"\t";
-        }
-        cout<<"\n";
-    }   
-}
 int main()
 {
-    Matrix A,B;
-    cout<<"\nMatrix A\t:\n";
-    A.create();
-    A.display();
-    cout<<"\nMatrix B\t:\n";
-    B.create();
-    B.display();
+    int a,b;
+    cout<<"Enter two numbers\t: ";
+    cin>>a>>b;
+    test t(a,b);
     try
     {
-        cout<<"\nA + B = ";
-        A+B;
+        cout<<t.devide();
     }
-    catch(char const* s)
+    catch(char const* a)
     {
-        cout<<"\nProgram terminated\n"<<s<<endl;
+        cout<<a<<"\nProgram terminated"<<endl;
     }
+    cout<<endl;
     return 0;
 }
